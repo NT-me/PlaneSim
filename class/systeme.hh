ab432b36a5b7a329355c13ae0572063149e5de2f
gnouf1 <jeuxforum26@gmail.com>
2019-12-27 18:56:58 +0100

    constructeur systeme OK

--- class/systeme.hh
+++ class/systeme.hh
@@ -14,34 +14,39 @@ class systeme{
   */
 
 private:
-  // Création des pompes
-  pompe* p11 = new pompe(11, true, 'N');
-  pompe* p12 = new pompe(12, false, 'S');
 
-  pompe* p21 = new pompe(21, true, 'N');
-  pompe* p22 = new pompe(22, false, 'S');
+  //   pompes
+  pompe* p11;
+  pompe* p12;
 
-  pompe* p31 = new pompe(31, true, 'N');
-  pompe* p32 = new pompe(32, false, 'S');
+  pompe* p21;
+  pompe* p22;
 
-  // Création des réservoirs
-  tank* t1 = new tank(1, true, *p11, *p12);
-  tank* t2 = new tank(2, true, *p21, *p22);
-  tank* t3 = new tank(3, true, *p31, *p32);
+  pompe* p31;
+  pompe* p32 ;
 
-  // Création des moteurs
-  engine* e1 = new engine(1, true);
-  engine* e2 = new engine(2, true);
-  engine* e3 = new engine(3, true);
+  //   réservoirs
+  tank* t1;
+  tank* t2;
+  tank* t3;
 
-  // Création des vannes
-  vanne* vt12 = new vanne(120, false, "T1", "T2");
-  vanne* vt23 = new vanne(230, false, "T2", "T3");
+  //   moteurs
+  engine* e1;
+  engine* e2;
+  engine* e3;
 
-  vanne* v12 = new vanne(12, false, "T1", "E2");
-  vanne* v13 = new vanne(13, false, "T1", "E3");
-  vanne* v23 = new vanne(23, false, "T2", "E3");
+  //   vannes
+  vanne* vt12; // Id avec un 0 en plus à la fin
+  vanne* vt23; // Id avec un 0 en plus à la fin
 
+  vanne* v12;
+  vanne* v13;
+  vanne* v23;
 
 public:
+  //Constructeur
+  systeme();
+
+  //Destructeur
+  ~systeme();
 };
