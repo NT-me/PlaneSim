78f179a80ea83a577518dc138c8c2f265b0fc1fb
gnouf1 <jeuxforum26@gmail.com>
2019-12-30 18:12:09 +0100

    petit nettoyage + début sauvegarde

---
+++ class/sauvegarde.hh
@@ -0,0 +1,17 @@
+#include <cstring>
+#include <iostream>
+#include "systeme.hh"
+
+using namespace std;
+
+class sauvegarde{
+  /*
+    L'objet sauvegarde permet d'enrengistrer l'historique et la note.
+  */
+  private:
+    int note; // entre 0 et 10
+    string historique;
+    systeme S_Save;
+
+  public:
+};
